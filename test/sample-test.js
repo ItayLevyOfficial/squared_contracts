aab0d871608d89e3eae20b4341dc5f5802b524c7
Itay Levy <itaylevy134@gmail.com>
2022-01-25 21:12:24 +0200

    Now the code almost compiles

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,18 +3,18 @@ const {ethers} = require('hardhat')
 
 describe('Greeter', function () {
   it('Should return the new greeting once it\'s changed', async function () {
-    const Greeter = await ethers.getContractFactory('Greeter')
+    const Greeter = await ethers.getContractFactory('DefiRound')
     const greeter = await Greeter.deploy('Hello, world!')
     await greeter.deployed()
 
-    expect(await greeter.greet()).to.equal('Hello, world!')
-
-    const setGreetingTx = await greeter.setGreeting('Hola, mundo!')
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait()
-
-    expect(await greeter.greet()).to.equal('Hola, mundo!')
+    // expect(await greeter.greet()).to.equal('Hello, world!')
+    //
+    // const setGreetingTx = await greeter.setGreeting('Hola, mundo!')
+    //
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait()
+    //
+    // expect(await greeter.greet()).to.equal('Hola, mundo!')
   })
 })
 
