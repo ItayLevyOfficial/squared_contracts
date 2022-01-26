dbc2ef0128b7e3450c245c80023c24230e140071
Itay Levy <itaylevy134@gmail.com>
2022-01-26 16:19:51 +0200

    Now the sample-test.js deploys the contract in the beforeeach section

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,15 +1,19 @@
 const {expect} = require('chai')
 const {ethers} = require('hardhat')
 
+let defiRound;
+
 describe('Greeter', function () {
-  it('Should return the new greeting once it\'s changed', async function () {
+  beforeEach(async () => {
     const DefiRound = await ethers.getContractFactory('DefiRound')
     const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
     const treasuryWallet = ethers.Wallet.createRandom()
     const treasury = treasuryWallet.address
-    const defiRound = await DefiRound.deploy(WETH, treasury, 1000)
+    defiRound = await DefiRound.deploy(WETH, treasury, 1000)
     await defiRound.deployed()
-    
+  })
+  it('Should return the new greeting once it\'s changed', async function () {
+    // await defiRound.deposit()
     // expect(await greeter.greet()).to.equal('Hello, world!')
     //
     // const setGreetingTx = await greeter.setGreeting('Hola, mundo!')
