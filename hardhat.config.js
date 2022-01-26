31a5b38763a30fc5513bbccd680ec7c4cf586e2b
Itay Levy <itaylevy134@gmail.com>
2022-01-26 15:33:17 +0200

    Now the tokemak contract getting deployed to the hardhat local network great

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.6.11",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true
+    }
+  }
 };
