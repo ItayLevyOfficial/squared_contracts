2c7d662b5afd85bd9c656fbd0b935f9e90ef2772
Itay Levy <itaylevy134@gmail.com>
2022-01-26 11:38:30 +0200

    comments back

--- test/sample-test.js
+++ test/sample-test.js
@@ -6,6 +6,15 @@ describe('Greeter', function () {
     const DefiRound = await ethers.getContractFactory('DefiRound')
     const defiRound = await DefiRound.deploy('Hello, world!')
     await defiRound.deployed()
+
+    // expect(await greeter.greet()).to.equal('Hello, world!')
+    //
+    // const setGreetingTx = await greeter.setGreeting('Hola, mundo!')
+    //
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait()
+    //
+    // expect(await greeter.greet()).to.equal('Hola, mundo!')
   })
 })
 
