50757bfd261993be095645987f9ed0789d32bb1c
Itay Levy <itaylevy134@gmail.com>
2022-01-26 13:05:23 +0200

    created the WETH parameter

--- test/sample-test.js
+++ test/sample-test.js
@@ -4,7 +4,8 @@ const {ethers} = require('hardhat')
 describe('Greeter', function () {
   it('Should return the new greeting once it\'s changed', async function () {
     const DefiRound = await ethers.getContractFactory('DefiRound')
-    const defiRound = await DefiRound.deploy('Hello, world!')
+    const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+    const defiRound = await DefiRound.deploy(WETH)
     await defiRound.deployed()
 
     // expect(await greeter.greet()).to.equal('Hello, world!')
