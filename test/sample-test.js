af9c52396810d2f825164524f1a85acb06aac82a
Itay Levy <itaylevy134@gmail.com>
2022-01-26 15:20:38 +0200

    Now the error message is because the contract size

--- test/sample-test.js
+++ test/sample-test.js
@@ -5,9 +5,11 @@ describe('Greeter', function () {
   it('Should return the new greeting once it\'s changed', async function () {
     const DefiRound = await ethers.getContractFactory('DefiRound')
     const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-    const defiRound = await DefiRound.deploy(WETH)
+    const treasuryWallet = ethers.Wallet.createRandom()
+    const treasury = treasuryWallet.address
+    const defiRound = await DefiRound.deploy(WETH, treasury, 1000)
     await defiRound.deployed()
-
+    
     // expect(await greeter.greet()).to.equal('Hello, world!')
     //
     // const setGreetingTx = await greeter.setGreeting('Hola, mundo!')
