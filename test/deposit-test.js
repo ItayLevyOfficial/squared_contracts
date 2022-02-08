5e206c85128bb22cfaa80734e20ff3534ad35bc9
The dragon <mrbeesizzle@gmail.com>
2022-02-08 13:26:45 +0200

    created the utils js file

--- test/deposit-test.js
+++ test/deposit-test.js
@@ -8,9 +8,6 @@ const hashAddress = (address) =>
     'hex',
   )
 let defiRound
-const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-const ethPrice = 2599_46882140
-const maxTotalValue = ethPrice * 10
 
 const addWethToSupportedTokens = async () => {
   const ethereumChainlinkAddress = '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419'
--- test/utils.js
+++ test/utils.js
@@ -1,8 +1,15 @@
 const { ethers } = require('ethers')
+const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+const ethPrice = 2599_46882140
+const maxTotalValue = ethPrice * 10
 
 const deployLaunchContract = async () => {
   const DefiRound = await ethers.getContractFactory('DefiRound')
   const treasuryWallet = ethers.Wallet.createRandom()
   const treasury = treasuryWallet.address
   return await DefiRound.deploy(WETH, treasury, maxTotalValue)
+}
+
+module.exports = {
+  WETH, ethPrice, maxTotalValue, deployLaunchContract
 }
