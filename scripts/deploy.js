3de4e1d68c91c29e4ee38d66f93be79619314288
The dragon <mrbeesizzle@gmail.com>
2022-02-09 16:34:33 +0200

    Now the deploy script add 100 ether to my metamask wallet address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -17,7 +17,7 @@ const main = async () => {
   const myMetamaskWalletAddress = '0xA3318B6027DC8fC382F990Bee9d2308E2ea3a388'
   await network.provider.send("hardhat_setBalance", [
     myMetamaskWalletAddress,
-    ethers.utils.parseEther('100')
+    ethers.utils.parseEther('10').toHexString()
   ])
 }
 
