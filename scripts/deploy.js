d9785e79d3d58ab4d9ade90abf5b70d4461b9b94
The dragon <mrbeesizzle@gmail.com>
2022-02-09 15:34:48 +0200

    created the tokens.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,13 +4,15 @@ const main = async () => {
   const DefiRound = await ethers.getContractFactory('DefiRound')
   const treasuryWallet = ethers.Wallet.createRandom()
   const treasury = treasuryWallet.address
-  const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+  const nativeTokenWrapper = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+  const stableToken = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
   const ethPrice = 2599_46882140
   const maxTotalValue = ethPrice * 10
 
-  defiRound = await DefiRound.deploy(WETH, treasury, maxTotalValue)
+  const defiRound = await DefiRound.deploy(nativeTokenWrapper, treasury, maxTotalValue)
   const contractAddress = (await defiRound.deployed()).address
   console.table({ contractAddress })
+  const tx = await defiRound.
 }
 
 main()
---
+++ test/tokens.js
@@ -0,0 +1,7 @@
+const tokens = {
+  wrappedEther: {},
+  usdc: {
+    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    chainlinkAddress: '0x8fffffd4afb6115b954bd326cbe7b4ba576818f6',
+  },
+}
