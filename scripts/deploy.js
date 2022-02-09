b4cd0409d28d8885a7f8b81c63c70630ff8a0f85
The dragon <mrbeesizzle@gmail.com>
2022-02-09 16:02:35 +0200

    Now the deploy script looks better

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,19 @@
 const { ethers } = require('hardhat')
+const { selectedChain } = require('../test/chains')
+const { supportNativeToken, supportStableToken } = require('../test/utils')
 
 const main = async () => {
   const DefiRound = await ethers.getContractFactory('DefiRound')
   const treasuryWallet = ethers.Wallet.createRandom()
   const treasury = treasuryWallet.address
-  const nativeTokenWrapper = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-  const stableToken = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-  const ethPrice = 2599_46882140
-  const maxTotalValue = ethPrice * 10
+  // Need extra 8 zeros for the decimals.
+  const maxTotalValue = 100_000_00000000
 
-  const defiRound = await DefiRound.deploy(nativeTokenWrapper, treasury, maxTotalValue)
+  const defiRound = await DefiRound.deploy(selectedChain.nativeToken, treasury, maxTotalValue)
   const contractAddress = (await defiRound.deployed()).address
   console.table({ contractAddress })
-  const tx = await defiRound.
+  await supportNativeToken(defiRound)
+  await supportStableToken(defiRound)
 }
 
 main()
