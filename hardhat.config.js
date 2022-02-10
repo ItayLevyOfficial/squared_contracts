6392ca81d623fecb4577cb0b49551568fa9335d7
The dragon <mrbeesizzle@gmail.com>
2022-02-10 17:28:08 +0200

    updated the chainid

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,6 +20,7 @@ module.exports = {
   solidity: "0.6.11",
   networks: {
     hardhat: {
+      chainId: 1337,
       allowUnlimitedContractSize: true,
       forking: {
         url: "https://eth-mainnet.alchemyapi.io/v2/2wzUWNwC9bpWNB5tNUznTTL0yogzqamW",
