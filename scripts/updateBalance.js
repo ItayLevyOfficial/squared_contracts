9cad9f0fbfb1631c0a4e4a21c9371560c975a973
The dragon <mrbeesizzle@gmail.com>
2022-02-14 10:35:05 -0500

    Now the update balance script works great

--- scripts/updateBalance.js
+++ scripts/updateBalance.js
@@ -4,23 +4,26 @@ const { selectedChain } = require('../test/chains')
 
 const main = async () => {
   const myMetamaskWalletAddress = '0x079DD7D40051831875C274D190FED9D8db4c135b'
-  console.log('a');
   await network.provider.send('hardhat_setBalance', [
     myMetamaskWalletAddress,
     ethers.utils.parseEther('10').toHexString(),
   ])
-
   const erc20abi = require('./erc20abi.json')
+  await network.provider.request({
+    method: 'hardhat_impersonateAccount',
+    params: [myMetamaskWalletAddress],
+  })
   const signer = await ethers.getSigner(myMetamaskWalletAddress)
   const stableCoinContract = new Contract(
     selectedChain.stableToken.address,
     erc20abi,
     signer,
   )
-
-  console.log('b');
   await stableCoinContract.gimmeSome()
-  console.log('c');
+  await network.provider.request({
+    method: 'hardhat_stopImpersonatingAccount',
+    params: [myMetamaskWalletAddress],
+  })
 }
 
 main()
