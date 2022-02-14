d78bcab204288f36a541fe7bec96e5ff6cf67921
The dragon <mrbeesizzle@gmail.com>
2022-02-14 11:09:31 -0500

    created the contrants file

--- scripts/updateBalance.js
+++ scripts/updateBalance.js
@@ -1,9 +1,9 @@
 const { Contract } = require('ethers')
 const { ethers, network } = require('hardhat')
 const { selectedChain } = require('../test/chains')
+const { myMetamaskWalletAddress } = require('./constants')
 
 const main = async () => {
-  const myMetamaskWalletAddress = '0x079DD7D40051831875C274D190FED9D8db4c135b'
   await network.provider.send('hardhat_setBalance', [
     myMetamaskWalletAddress,
     ethers.utils.parseEther('10').toHexString(),
--- test/utils.js
+++ test/utils.js
@@ -51,5 +51,5 @@ module.exports = {
   maxTotalValue,
   deployLaunchContract,
   supportNativeToken,
-  supportStableToken,
+  supportStableToken
 }
