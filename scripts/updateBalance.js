5312ac4d8fd1018c3d2dec9b9eedc38aabdaae38
The dragon <mrbeesizzle@gmail.com>
2022-02-14 10:15:36 -0500

    removed redundant code from script

--- scripts/updateBalance.js
+++ scripts/updateBalance.js
@@ -1,15 +1,6 @@
-const { BigNumber, Contract } = require('ethers')
+const { Contract } = require('ethers')
 const { ethers, network } = require('hardhat')
 const { selectedChain } = require('../test/chains')
-
-const toBytes32 = (bn) => {
-  return ethers.utils.hexlify(ethers.utils.zeroPad(bn.toHexString(), 32))
-}
-
-const setStorageAt = async (address, index, value) => {
-  await ethers.provider.send('hardhat_setStorageAt', [address, index, value])
-  await ethers.provider.send('evm_mine', []) // Just mines to the next block
-}
 
 const main = async () => {
   const myMetamaskWalletAddress = '0x079DD7D40051831875C274D190FED9D8db4c135b'
@@ -17,19 +8,6 @@ const main = async () => {
     myMetamaskWalletAddress,
     ethers.utils.parseEther('10').toHexString(),
   ])
-  // const stableCoinBalance = ethers.BigNumber.from(100_000).mul(
-  //   BigNumber.from(10).pow(BigNumber.from(selectedChain.stableToken.decimals)),
-  // )
-  // const index = ethers.utils.solidityKeccak256(
-  //   ['uint256', 'uint256'],
-  //   [myMetamaskWalletAddress, selectedChain.stableToken.slot],
-  // )
-
-  // await setStorageAt(
-  //   selectedChain.stableToken.address,
-  //   index.toString(),
-  //   toBytes32(stableCoinBalance).toString(),
-  // )
 
   const erc20abi = require('./erc20abi.json')
   const signer = await ethers.getSigner(myMetamaskWalletAddress)
--- test/chains.js
+++ test/chains.js
@@ -5,7 +5,7 @@ const chains = {
       chainlinkAddress: '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419',
     },
     stableToken: {
-      address: '0x6F6f570F45833E249e27022648a26F4076F48f78',
+      address: '0xF32D39ff9f6Aa7a7A64d7a4F00a54826Ef791a55',
       chainlinkAddress: '0x8fffffd4afb6115b954bd326cbe7b4ba576818f6',
       decimals: 6,
       slot: 9 // To update the balance manually on hardhat network
