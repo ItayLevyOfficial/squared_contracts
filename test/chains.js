73ae19b53b433e8b7777047e43d7b7f4ddfd5afa
The dragon <mrbeesizzle@gmail.com>
2022-02-17 15:01:23 -0500

    updated the usdc address

--- test/chains.js
+++ test/chains.js
@@ -10,7 +10,7 @@ const chains = {
       chainlinkAddress: '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419',
     },
     stableToken: {
-      address: '0xb9bEECD1A582768711dE1EE7B0A1d582D9d72a6C',
+      address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
       chainlinkAddress: '0x8fffffd4afb6115b954bd326cbe7b4ba576818f6',
       decimals: 6,
       slot: 9, // To update the balance manually on hardhat network
