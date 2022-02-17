abad5eae38631fc24aab28ef20367532208df77e
The dragon <mrbeesizzle@gmail.com>
2022-02-17 13:54:53 -0500

    moved the fork data to the selected chain config

--- test/chains.js
+++ test/chains.js
@@ -1,5 +1,10 @@
 const chains = {
   ethereum: {
+    forkData: {
+      url:
+        'https://eth-mainnet.alchemyapi.io/v2/2wzUWNwC9bpWNB5tNUznTTL0yogzqamW',
+      blockNumber: 14101114,
+    },
     nativeToken: {
       address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
       chainlinkAddress: '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419',
@@ -8,9 +13,9 @@ const chains = {
       address: '0xb9bEECD1A582768711dE1EE7B0A1d582D9d72a6C',
       chainlinkAddress: '0x8fffffd4afb6115b954bd326cbe7b4ba576818f6',
       decimals: 6,
-      slot: 9 // To update the balance manually on hardhat network
-    }
-  }
+      slot: 9, // To update the balance manually on hardhat network
+    },
+  },
 }
 
-module.exports = { selectedChain : chains.ethereum }
+module.exports = { selectedChain: chains.ethereum }
