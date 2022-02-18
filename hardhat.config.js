006f3572a7ff8494f3f6ce893ab4f28a57597f6e
The dragon <mrbeesizzle@gmail.com>
2022-02-18 12:47:54 -0500

    Now we can try to deploy to the bsc testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 const { selectedChain } = require('./test/chains')
-const { mnemonic } = require('./secrets.json')
+const { mnemonic } = require('./.secrets.json')
 require('@nomiclabs/hardhat-waffle')
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
