1553aeafff7218e782f820e87b25fb7e071fca01
The dragon <mrbeesizzle@gmail.com>
2022-02-18 12:03:42 -0500

    Now the update balance function is great again

--- scripts/updateBalance.js
+++ scripts/updateBalance.js
@@ -1,35 +1,30 @@
+const { Contract } = require('ethers')
 const { ethers, network } = require('hardhat')
 const { selectedChain } = require('../test/chains')
 const { myMetamaskWalletAddress } = require('./constants')
-const { BigNumber } = require('ethers')
-
-const toBytes32 = (bn) => {
-  return ethers.utils.hexlify(ethers.utils.zeroPad(bn.toHexString(), 32))
-}
-
-const setStorageAt = async (address, index, value) => {
-  await ethers.provider.send('hardhat_setStorageAt', [address, index, value])
-  await ethers.provider.send('evm_mine', []) // Just mines to the next block
-}
 
 const main = async () => {
   await network.provider.send('hardhat_setBalance', [
     myMetamaskWalletAddress,
     ethers.utils.parseEther('10').toHexString(),
   ])
-  const stableCoinBalance = ethers.BigNumber.from(100_000).mul(
-    BigNumber.from(10).pow(BigNumber.from(selectedChain.stableToken.decimals)),
-  )
-  const index = ethers.utils.solidityKeccak256(
-    ['uint256', 'uint256'],
-    [myMetamaskWalletAddress, selectedChain.stableToken.slot],
-  )
 
-  await setStorageAt(
+  const erc20abi = require('./erc20abi.json')
+  await network.provider.request({
+    method: 'hardhat_impersonateAccount',
+    params: [myMetamaskWalletAddress],
+  })
+  const signer = await ethers.getSigner(myMetamaskWalletAddress)
+  const stableCoinContract = new Contract(
     selectedChain.stableToken.address,
-    index.toString(),
-    toBytes32(stableCoinBalance).toString(),
+    erc20abi,
+    signer,
   )
+  await stableCoinContract.gimmeSome()
+  await network.provider.request({
+    method: 'hardhat_stopImpersonatingAccount',
+    params: [myMetamaskWalletAddress],
+  })
 }
 
 main()
