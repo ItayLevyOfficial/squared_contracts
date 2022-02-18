b2bb621dd69f66ec2e6f9db99d2bc662a3ab3eaf
The dragon <mrbeesizzle@gmail.com>
2022-02-18 12:43:43 -0500

    now the hardhat config file look fine to work with bsc testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 const { selectedChain } = require('./test/chains')
-
+const { mnemonic } = require('./secrets.json')
 require('@nomiclabs/hardhat-waffle')
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,11 +19,24 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: '0.6.11',
+  defaultNetwork: 'bscTestnet',
   networks: {
     hardhat: {
       chainId: 1337,
       allowUnlimitedContractSize: true,
       forking: selectedChain.forkData,
+    },
+    bscTestnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: mnemonic },
+    },
+    bscMainnet: {
+      url: 'https://bsc-dataseed.binance.org/',
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: mnemonic },
     },
   },
 }
