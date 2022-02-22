1b1428fdc249c8b66c15d5cb67c6e5c5a7281b73
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-02-22 16:47:25 -0500

    refactor testTokens

--- scripts/deployTestTokens.js
+++ scripts/deployTestTokens.js
@@ -1,20 +1,12 @@
 const { ethers } = require('hardhat')
 
-const main = async () => {
-  const SqrdLpROUND = await ethers.getContractFactory('FakeSQRDLP')
-  const sqrdLpROUND = await SqrdLpROUND.deploy()
-  const sqrdLpContractAddress = (await sqrdLpROUND.deployed()).address
-  console.log('Fake SQRD LP:', sqrdLpContractAddress)
-
-  const SqrdRound = await ethers.getContractFactory('FakeSQRD')
-  const sqrdRound = await SqrdRound.deploy()
-  const sqrdContractAddress = (await sqrdRound.deployed()).address
-  console.log('Fake SQRD:', sqrdContractAddress)
-
-  const UsdcRound = await ethers.getContractFactory('FakeUSDC')
-  const usdcRound = await UsdcRound.deploy()
-  const usdcContractAddress = (await usdcRound.deployed()).address
-  console.log('Fake USDC:', usdcContractAddress)
+const main = async (contract) => {
+  const poolRound = await ethers.getContractFactory(contract)
+  const PoolRound = await poolRound.deploy()
+  const poolContractAddress = (await PoolRound.deployed()).address
+  console.log(`${contract} address:`, poolContractAddress)
 }
 
-main()
+main('FakeSQRDLP')
+main('FakeSQRD')
+main('FakeUSDC')
