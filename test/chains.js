120e8d1ba8ab4ee9cb61501db74bac648bffab47
The dragon <mrbeesizzle@gmail.com>
2022-02-23 13:03:44 -0500

    it should work

--- test/chains.js
+++ test/chains.js
@@ -1,7 +1,7 @@
 const chains = {
   ethereum: {
     nativeToken: {
-      address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      address: '0xB0f05d25e41FbC2b52013099ED9616f1206Ae21B',
       chainlinkAddress: '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419',
       decimals: 18,
       name: 'ETH'
@@ -20,7 +20,7 @@ const chains = {
       name: 'SQRD'
     },
     sqrdLpToken: {
-      address: '0xb9bEECD1A582768711dE1EE7B0A1d582D9d72a6C',
+      address: '0x8A93d247134d91e0de6f96547cB0204e5BE8e5D8',
       decimals: 6,
       slot: 9, // To update the balance manually on hardhat network
       name: 'SQRD_LP'
