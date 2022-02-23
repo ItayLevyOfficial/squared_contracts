367be89002953142923fe18eaf7ae0108e324e81
The dragon <mrbeesizzle@gmail.com>
2022-02-23 04:40:28 -0500

    updated the mnemonic

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require('@nomiclabs/hardhat-waffle')
 require('@openzeppelin/hardhat-upgrades')
-
+const { mnemonic } = require('./secrets.json')
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
@@ -36,9 +36,16 @@ module.exports = {
       chainId: 1337,
       allowUnlimitedContractSize: true,
       forking: {
-        url: 'https://eth-mainnet.alchemyapi.io/v2/2wzUWNwC9bpWNB5tNUznTTL0yogzqamW',
+        url:
+          'https://eth-mainnet.alchemyapi.io/v2/2wzUWNwC9bpWNB5tNUznTTL0yogzqamW',
         blockNumber: 14101114,
       },
+    },
+    bscTestnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: mnemonic },
     },
   },
 }
