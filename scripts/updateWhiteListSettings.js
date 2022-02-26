1b88875bcfb8c806708db14320ec30738dba1489
The dragon <mrbeesizzle@gmail.com>
2022-02-26 18:51:15 -0500

    now the update white list configuration works great

--- scripts/updateWhiteListSettings.js
+++ scripts/updateWhiteListSettings.js
@@ -1,8 +1,14 @@
+const { configureHashedWhitelist } = require('../test/utils')
+const { ethers } = require('hardhat')
+
 const main = async () => {
-  const whitelistedUsers = require('./whitelistedUsers.json').map((buffer) =>
-    Buffer.from(buffer.data),
+  const hashedWhitelistedUsers = require('./hashedWhitelistedUsers.json').map(
+    (buffer) => Buffer.from(buffer.data),
   )
-  console.log({ whitelistedUsers })
+  const MyContract = await ethers.getContractFactory('DefiRound')
+  const launchContractAddress = '0xF32D39ff9f6Aa7a7A64d7a4F00a54826Ef791a55'
+  const defiRound = await MyContract.attach(launchContractAddress)
+  await configureHashedWhitelist(hashedWhitelistedUsers, defiRound)
 }
 
 main()
