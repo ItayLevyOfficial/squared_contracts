d1ac04c8be604ec92ad6c1d07c97dec9c02d8aa7
The dragon <mrbeesizzle@gmail.com>
2022-02-26 18:57:03 -0500

    now it should work great

--- scripts/moveStage.js
+++ scripts/moveStage.js
@@ -3,8 +3,7 @@ const { selectedChain } = require('../test/chains')
 
 const main = async () => {
   const MyContract = await ethers.getContractFactory('DefiRound')
-  const launchContractAddress = '0x976fcd02f7C4773dd89C309fBF55D5923B4c98a1'
-  const contract = await MyContract.attach(launchContractAddress)
+  const contract = await MyContract.attach(selectedChain.launchContractAddress)
   const rates = [
     {
       token: selectedChain.stableToken.address,
