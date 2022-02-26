22bcf27867bb99438d5c9d01bb477be5303c7c5e
The dragon <mrbeesizzle@gmail.com>
2022-02-26 18:56:10 -0500

    now the launch contract addres being fetched

--- scripts/updateWhiteListSettings.js
+++ scripts/updateWhiteListSettings.js
@@ -1,13 +1,13 @@
 const { configureHashedWhitelist } = require('../test/utils')
 const { ethers } = require('hardhat')
+const { selectedChain } = require('../test/chains')
 
 const main = async () => {
   const hashedWhitelistedUsers = require('./hashedWhitelistedUsers.json').map(
     (buffer) => Buffer.from(buffer.data),
   )
   const MyContract = await ethers.getContractFactory('DefiRound')
-  const launchContractAddress = '0xF32D39ff9f6Aa7a7A64d7a4F00a54826Ef791a55'
-  const defiRound = await MyContract.attach(launchContractAddress)
+  const defiRound = await MyContract.attach(selectedChain.launchContractAddress)
   await configureHashedWhitelist(hashedWhitelistedUsers, defiRound)
 }
 
