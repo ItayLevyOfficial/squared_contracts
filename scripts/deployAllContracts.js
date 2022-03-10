b5e1364bc9529e8a4b33e969bf015674f8c49a0b
The dragon <mrbeesizzle@gmail.com>
2022-03-10 14:17:37 -0500

    Created a great file

--- scripts/deployAllContracts.js
+++ scripts/deployAllContracts.js
@@ -1,7 +1,14 @@
+const { ethers } = require('hardhat')
+const { selectedChain } = require('../test/chains')
+const { supportNativeToken, supportStableToken } = require('../test/utils')
+const { deployContract, deployPool } = require('./utils')
+
 const deployAllContracts = async (stableTokenAddress) => {
   const fakeSQRD = await deployContract({ contractName: 'FakeSQRD' })
   const fakeSQRDLP = await deployContract({ contractName: 'FakeSQRDLP' })
-  const fakeUSDC = await deployContract({ contractName: 'FakeUSDC' })
+  const actualStableTokenAddress =
+    stableTokenAddress ??
+    (await deployContract({ contractName: 'FakeUSDC' })).address
   const treasuryAddress = ethers.Wallet.createRandom().address
   // Need extra 8 zeros for the decimals.
   const maxTotalValue = 100_000_00000000
@@ -12,7 +19,7 @@ const deployAllContracts = async (stableTokenAddress) => {
   await supportNativeToken(deployedContract)
   await supportStableToken({
     defiRoundContract: deployedContract,
-    stableTokenAddress: fakeUSDC.address,
+    stableTokenAddress: actualStableTokenAddress,
   })
   await deployPool({
     tokenName: selectedChain.nativeToken.name,
@@ -21,7 +28,7 @@ const deployAllContracts = async (stableTokenAddress) => {
   })
   await deployPool({
     tokenName: selectedChain.stableToken.name,
-    tokenAddress: fakeUSDC.address,
+    tokenAddress: actualStableTokenAddress,
   })
   await deployPool({
     tokenName: selectedChain.sqrdToken.name,
@@ -32,3 +39,5 @@ const deployAllContracts = async (stableTokenAddress) => {
     tokenAddress: fakeSQRDLP.address,
   })
 }
+
+module.exports = { deployAllContracts }
