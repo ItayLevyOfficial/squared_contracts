b059a8c6b9970af6f45b675b0fa1681bf2aed757
The dragon <mrbeesizzle@gmail.com>
2022-03-10 14:21:12 -0500

    refactored the deployTestnetContract script

--- scripts/deployTestnetContract.js
+++ scripts/deployTestnetContract.js
@@ -1,40 +1,8 @@
-const { ethers } = require('hardhat')
 const { selectedChain } = require('../test/chains')
-const { supportNativeToken, supportStableToken } = require('../test/utils')
-const { deployContract, deployPool } = require('./utils')
+const { deployAllContracts } = require('./deployAllContracts')
 
 const main = async () => {
-  const treasuryAddress = ethers.Wallet.createRandom().address
-  // Need extra 8 zeros for the chainlink usd response decimals.
-  const maxTotalValue = 100_000_00000000
-  const deployedContract = await deployContract({
-    contractName: 'DefiRound',
-    params: [selectedChain.nativeToken.address, treasuryAddress, maxTotalValue],
-  })
-  await supportNativeToken(deployedContract)
-  await supportStableToken({
-    defiRoundContract: deployedContract,
-    stableTokenAddress: selectedChain.stableToken.address,
-  })
-  const fakeSQRD = await deployContract({ contractName: 'FakeSQRD' })
-  const fakeSQRDLP = await deployContract({ contractName: 'FakeSQRDLP' })
-  await deployPool({
-    tokenName: selectedChain.nativeToken.name,
-    tokenAddress: selectedChain.nativeToken.address,
-    poolName: 'EthPool',
-  })
-  await deployPool({
-    tokenName: selectedChain.stableToken.name,
-    tokenAddress: selectedChain.stableToken.address,
-  })
-  await deployPool({
-    tokenName: selectedChain.sqrdToken.name,
-    tokenAddress: fakeSQRD.address,
-  })
-  await deployPool({
-    tokenName: selectedChain.sqrdLpToken.name,
-    tokenAddress: fakeSQRDLP.address,
-  })
+  await deployAllContracts(selectedChain.stableToken.address)
 }
 
 main()
